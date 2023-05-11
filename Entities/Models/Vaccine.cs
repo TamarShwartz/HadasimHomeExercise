e86a054b068bdde9a592e90aa49be638254a89c7
TamarShwartz <96349584+TamarShwartz@users.noreply.github.com>
2023-05-11 21:04:11 +0300

    add project

---
+++ Entities/Models/Vaccine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project.Models
+{
+    public partial class Vaccine
+    {
+        public Vaccine()
+        {
+            ClientVaccines = new HashSet<ClientVaccine>();
+            CoronaTests = new HashSet<CoronaTest>();
+        }
+
+        public int Id { get; set; }
+        public DateTime? ProductionDate { get; set; }
+        public string ManufacturerName { get; set; }
+
+        public virtual ICollection<ClientVaccine> ClientVaccines { get; set; }
+        public virtual ICollection<CoronaTest> CoronaTests { get; set; }
+    }
+}
